d673309b296cf3e35fb13703bd2b1636ae241bad
Agon20 <azemi.agon20@gmail.com>
2023-06-29 13:00:42 +0200

    UE8 Aufgabe 1 & 2 jetzt wirklich fertig

--- src/UE8/Aufgabe2/SnakeIterator2DArray.java
+++ src/UE8/Aufgabe2/SnakeIterator2DArray.java
@@ -9,14 +9,15 @@ public class SnakeIterator2DArray<T> implements Iterator<T> {
     private int lastarrayfieldlength;
     private int arraylength2field;
     private int current1field;
+    private int current1field2;
     private int current2field;
     private boolean direction;
 
     public SnakeIterator2DArray(T[][] array){
         this.array = array;
         arraylength1field = array.length;
-        lastarrayfieldlength = array.length;
-        //arraylength2field = array[0].length;
+        //lastarrayfieldlength = array.length;
+        lastarrayfieldlength = array[0].length;
         current1field = 0;
         current2field = 0;
         direction = true;
@@ -30,21 +31,20 @@ public class SnakeIterator2DArray<T> implements Iterator<T> {
     @Override
     public T next() {
         arraylength2field = array[current1field].length;
+        if (lastarrayfieldlength != arraylength2field){
+            lastarrayfieldlength = arraylength2field;
+            if (current2field != 0){current2field = arraylength2field-1;}
+        }
         T value = array[current1field][current2field];
 
         if (direction){
             if (current2field == arraylength2field -1){
-                current2field = arraylength2field-1;
                 current1field++;
                 direction = false;
             }else {
                 current2field++;
             }
         }else{
-            if (lastarrayfieldlength != arraylength2field){
-                lastarrayfieldlength = arraylength2field;
-                current2field = arraylength2field;
-            }
             if (current2field == 0){
                 current1field++;
                 direction = true;
@@ -57,7 +57,7 @@ public class SnakeIterator2DArray<T> implements Iterator<T> {
         return value;
     }
         public static void main(String[] args) {
-            Integer[][] array = {{1, 2, 3, 4}, {5, 6, 7, 8, 9, 9}, {10, 11, 12, 13}};
+            Integer[][] array = {{1, 2, 3, 4, 5,}, {9, 8, 7, 6}, {10, 11, 12, 13, 14 , 15, 16, 17},{20, 19, 18}};
             SnakeIterator2DArray<Integer> iterator = new SnakeIterator2DArray<>(array);
 
             while (iterator.hasNext()) {
